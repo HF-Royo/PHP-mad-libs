d5c6367597a6800584dff4e43e64a720bb1d457c
HF-Royo <74603650+HF-Royo@users.noreply.github.com>
2022-10-22 19:55:02 +0800

    Create site.php

---
+++ site.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    < ! ╌ site.php is a file name╌>
+    <form action="site.php" method="get">
+
+        Color: <input type="text" name="color"> <br><br>
+        Plural Noun: <input type="text" name="PluralNoun"> <br><br>
+        Celebrity: <input type="text" name="celebrity"> <br><br>
+
+        <input type="submit">
+    </form>
+    <br><br>
+    <?php
+
+    $color = $_GET["color"];
+    $PluralNoun = $_GET["PluralNoun"];
+    $celebrity = $_GET["celebrity"];
+    echo "Roses are $color <br>";
+    echo "$PluralNoun are blue <br>";
+    echo "I love $celebrity <br>";
+
+    ?>
+
+  </body>
+</html>
